79fe0d753cfc49261b3a95b0c05ccf7368716827
agutnova <44023289+agutnova@users.noreply.github.com>
2021-11-08 08:47:20 +0300

    Add files via upload

--- Calc/Calc/Form1.Designer.cs
+++ Calc/Calc/Form1.Designer.cs
@@ -58,6 +58,7 @@
             this.tbResult.Name = "tbResult";
             this.tbResult.Size = new System.Drawing.Size(221, 31);
             this.tbResult.TabIndex = 0;
+            this.tbResult.TabStop = false;
             this.tbResult.Text = "0";
             this.tbResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
@@ -67,11 +68,11 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(33, 33);
             this.button1.TabIndex = 1;
+            this.button1.TabStop = false;
             this.button1.Text = "7";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button9_Click);
-            this.button1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.button1_KeyPress);
-            this.button1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button1_KeyUp);
+            this.button1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button9_KeyUp);
             // 
             // button2
             // 
@@ -79,9 +80,11 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(33, 33);
             this.button2.TabIndex = 2;
+            this.button2.TabStop = false;
             this.button2.Text = "8";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button9_Click);
+            this.button2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button9_KeyUp);
             // 
             // button3
             // 
@@ -89,9 +92,11 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(33, 33);
             this.button3.TabIndex = 3;
+            this.button3.TabStop = false;
             this.button3.Text = "9";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button9_Click);
+            this.button3.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button9_KeyUp);
             // 
             // button4
             // 
@@ -99,9 +104,11 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(33, 33);
             this.button4.TabIndex = 6;
+            this.button4.TabStop = false;
             this.button4.Text = "6";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button9_Click);
+            this.button4.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button9_KeyUp);
             // 
             // button5
             // 
@@ -109,9 +116,11 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(33, 33);
             this.button5.TabIndex = 5;
+            this.button5.TabStop = false;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button9_Click);
+            this.button5.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button9_KeyUp);
             // 
             // button6
             // 
@@ -119,9 +128,11 @@
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(33, 33);
             this.button6.TabIndex = 4;
+            this.button6.TabStop = false;
             this.button6.Text = "4";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button9_Click);
+            this.button6.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button9_KeyUp);
             // 
             // button7
             // 
@@ -129,9 +140,11 @@
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(33, 33);
             this.button7.TabIndex = 9;
+            this.button7.TabStop = false;
             this.button7.Text = "3";
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button9_Click);
+            this.button7.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button9_KeyUp);
             // 
             // button8
             // 
@@ -139,9 +152,11 @@
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(33, 33);
             this.button8.TabIndex = 8;
+            this.button8.TabStop = false;
             this.button8.Text = "2";
             this.button8.UseVisualStyleBackColor = true;
             this.button8.Click += new System.EventHandler(this.button9_Click);
+            this.button8.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button9_KeyUp);
             // 
             // button9
             // 
@@ -149,9 +164,11 @@
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(33, 33);
             this.button9.TabIndex = 7;
+            this.button9.TabStop = false;
             this.button9.Text = "1";
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button9_Click);
+            this.button9.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button9_KeyUp);
             // 
             // bMult
             // 
@@ -159,9 +176,11 @@
             this.bMult.Name = "bMult";
             this.bMult.Size = new System.Drawing.Size(33, 33);
             this.bMult.TabIndex = 12;
+            this.bMult.TabStop = false;
             this.bMult.Text = "*";
             this.bMult.UseVisualStyleBackColor = true;
             this.bMult.Click += new System.EventHandler(this.bDiv_Click);
+            this.bMult.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button9_KeyUp);
             // 
             // bDiv
             // 
@@ -169,9 +188,11 @@
             this.bDiv.Name = "bDiv";
             this.bDiv.Size = new System.Drawing.Size(33, 33);
             this.bDiv.TabIndex = 11;
+            this.bDiv.TabStop = false;
             this.bDiv.Text = "/";
             this.bDiv.UseVisualStyleBackColor = true;
             this.bDiv.Click += new System.EventHandler(this.bDiv_Click);
+            this.bDiv.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button9_KeyUp);
             // 
             // button12
             // 
@@ -179,9 +200,11 @@
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(33, 33);
             this.button12.TabIndex = 10;
+            this.button12.TabStop = false;
             this.button12.Text = "0";
             this.button12.UseVisualStyleBackColor = true;
             this.button12.Click += new System.EventHandler(this.button9_Click);
+            this.button12.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button9_KeyUp);
             // 
             // bPlus
             // 
@@ -189,9 +212,11 @@
             this.bPlus.Name = "bPlus";
             this.bPlus.Size = new System.Drawing.Size(33, 33);
             this.bPlus.TabIndex = 13;
+            this.bPlus.TabStop = false;
             this.bPlus.Text = "+";
             this.bPlus.UseVisualStyleBackColor = true;
             this.bPlus.Click += new System.EventHandler(this.bDiv_Click);
+            this.bPlus.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button9_KeyUp);
             // 
             // bMinus
             // 
@@ -199,9 +224,11 @@
             this.bMinus.Name = "bMinus";
             this.bMinus.Size = new System.Drawing.Size(33, 33);
             this.bMinus.TabIndex = 14;
+            this.bMinus.TabStop = false;
             this.bMinus.Text = "-";
             this.bMinus.UseVisualStyleBackColor = true;
             this.bMinus.Click += new System.EventHandler(this.bDiv_Click);
+            this.bMinus.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button9_KeyUp);
             // 
             // bEquals
             // 
@@ -209,9 +236,11 @@
             this.bEquals.Name = "bEquals";
             this.bEquals.Size = new System.Drawing.Size(33, 33);
             this.bEquals.TabIndex = 15;
+            this.bEquals.TabStop = false;
             this.bEquals.Text = "=";
             this.bEquals.UseVisualStyleBackColor = true;
             this.bEquals.Click += new System.EventHandler(this.bEquals_Click);
+            this.bEquals.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button9_KeyUp);
             // 
             // bCLeft
             // 
@@ -220,9 +249,11 @@
             this.bCLeft.Name = "bCLeft";
             this.bCLeft.Size = new System.Drawing.Size(33, 33);
             this.bCLeft.TabIndex = 16;
+            this.bCLeft.TabStop = false;
             this.bCLeft.Text = "<-";
             this.bCLeft.UseVisualStyleBackColor = true;
             this.bCLeft.Click += new System.EventHandler(this.bCLeft_Click);
+            this.bCLeft.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button9_KeyUp);
             // 
             // bClearAll
             // 
@@ -230,9 +261,11 @@
             this.bClearAll.Name = "bClearAll";
             this.bClearAll.Size = new System.Drawing.Size(33, 33);
             this.bClearAll.TabIndex = 17;
+            this.bClearAll.TabStop = false;
             this.bClearAll.Text = "C";
             this.bClearAll.UseVisualStyleBackColor = true;
             this.bClearAll.Click += new System.EventHandler(this.bClearAll_Click);
+            this.bClearAll.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button9_KeyUp);
             // 
             // Form1
             // 
@@ -260,9 +293,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.Text = "Калькулятор";
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
-            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
-            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button9_KeyUp);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Calc/Calc/Form1.cs
+++ Calc/Calc/Form1.cs
@@ -15,6 +15,7 @@ namespace Calc
         char operation = ' ';
         bool ispressed = false;
         Int64 first = 0;
+        char digit = ' ';
         public Form1()
         {
             InitializeComponent();
@@ -22,7 +23,10 @@ namespace Calc
 
         private void button9_Click(object sender, EventArgs e)
         {
-            string t = ((Button)sender).Text;
+            string t = "";
+            if (digit == ' ') t = ((Button)sender).Text;
+            else t = ""+digit;
+            digit = ' ';
             if (tbResult.Text == "0" || ispressed)
             {
                 tbResult.Text = t;
@@ -35,7 +39,9 @@ namespace Calc
         {
             if (operation!=' ') bEquals_Click(sender, e);
             ispressed = true;
-            operation = ((Button)sender).Text[0];
+            if (digit == ' ') operation = ((Button)sender).Text[0];
+            else operation = digit;
+            digit = ' ';
             first = Convert.ToInt64(tbResult.Text);
         }
 
@@ -47,10 +53,7 @@ namespace Calc
                 case '-':tbResult.Text = Convert.ToString(first - Convert.ToInt64(tbResult.Text)); break;
                 case '*':tbResult.Text = Convert.ToString(first * Convert.ToInt64(tbResult.Text)); break;
                 case '/':if (tbResult.Text == "0")
-                    {
                         tbResult.Text = "На ноль делить нельзя!";
-                        ispressed = true;
-                    }
                     else tbResult.Text = Convert.ToString(first / Convert.ToInt64(tbResult.Text)); break;
             }
             operation = ' ';
@@ -72,29 +75,39 @@ namespace Calc
             ispressed = false;
         }
 
-        private void button1_KeyPress(object sender, KeyPressEventArgs e)
+        private void button9_KeyUp(object sender, KeyEventArgs e)
         {
-
-        }
-
-        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
-        {
-
-        }
-
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Form1_KeyUp(object sender, KeyEventArgs e)
-        {
-            
-        }
-
-        private void button1_KeyUp(object sender, KeyEventArgs e)
-        {
-
+            if ((e.KeyValue >= (int)Keys.D0 && e.KeyValue <= (int)Keys.D9) ||
+                    (e.KeyValue >= (int)Keys.NumPad0 && e.KeyValue <= (int)Keys.NumPad9))
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.D0: case Keys.NumPad0: digit = '0'; break;
+                    case Keys.D1: case Keys.NumPad1: digit = '1'; break;
+                    case Keys.D2: case Keys.NumPad2: digit = '2'; break;
+                    case Keys.D3: case Keys.NumPad3: digit = '3'; break;
+                    case Keys.D4: case Keys.NumPad4: digit = '4'; break;
+                    case Keys.D5: case Keys.NumPad5: digit = '5'; break;
+                    case Keys.D6: case Keys.NumPad6: digit = '6'; break;
+                    case Keys.D7: case Keys.NumPad7: digit = '7'; break;
+                    case Keys.D8: case Keys.NumPad8: digit = '8'; break;
+                    case Keys.D9: case Keys.NumPad9: digit = '9'; break;
+                }
+                button9_Click(sender, e);
+            }
+            if (e.KeyCode == Keys.Divide || e.KeyCode == Keys.Multiply ||
+                e.KeyCode == Keys.Add || e.KeyCode == Keys.Subtract) {
+                switch (e.KeyCode) {
+                    case Keys.Divide: digit = '/';break;
+                    case Keys.Multiply: digit = '*'; break;
+                    case Keys.Add: digit = '+'; break;
+                    case Keys.Subtract: digit = '-'; break;
+                }
+                bDiv_Click(sender, e);
+            }
+            if (e.KeyCode == Keys.Back) bCLeft_Click(sender, e);
+            if (e.KeyCode == Keys.C) bClearAll_Click(sender, e);
+            //if (e.KeyCode == Keys.Enter) bEquals_Click(sender, e);
         }
     }
 }
