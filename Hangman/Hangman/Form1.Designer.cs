56479c894d1ef9872f29771f42e5320c0d9f72dc
agutnova <44023289+agutnova@users.noreply.github.com>
2021-11-08 10:33:28 +0300

    Add files via upload

---
+++ Hangman/Hangman/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lMasked = new System.Windows.Forms.Label();
+            this.pbError = new System.Windows.Forms.PictureBox();
+            this.tbLetter = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lUsed = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbError)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lMasked
+            // 
+            this.lMasked.AutoSize = true;
+            this.lMasked.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lMasked.Location = new System.Drawing.Point(13, 71);
+            this.lMasked.Name = "lMasked";
+            this.lMasked.Size = new System.Drawing.Size(76, 25);
+            this.lMasked.TabIndex = 0;
+            this.lMasked.Text = "label1";
+            // 
+            // pbError
+            // 
+            this.pbError.Location = new System.Drawing.Point(12, 124);
+            this.pbError.Name = "pbError";
+            this.pbError.Size = new System.Drawing.Size(317, 286);
+            this.pbError.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbError.TabIndex = 1;
+            this.pbError.TabStop = false;
+            // 
+            // tbLetter
+            // 
+            this.tbLetter.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbLetter.Location = new System.Drawing.Point(104, 431);
+            this.tbLetter.Name = "tbLetter";
+            this.tbLetter.Size = new System.Drawing.Size(63, 29);
+            this.tbLetter.TabIndex = 2;
+            this.tbLetter.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbLetter_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 439);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите букву:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(188, 434);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Угадать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(15, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(266, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Область деятельности:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(341, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Использованные буквы";
+            // 
+            // lUsed
+            // 
+            this.lUsed.AutoSize = true;
+            this.lUsed.Location = new System.Drawing.Point(346, 157);
+            this.lUsed.Name = "lUsed";
+            this.lUsed.Size = new System.Drawing.Size(35, 13);
+            this.lUsed.TabIndex = 7;
+            this.lUsed.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 473);
+            this.Controls.Add(this.lUsed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbLetter);
+            this.Controls.Add(this.pbError);
+            this.Controls.Add(this.lMasked);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Виселица";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbError)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lMasked;
+        private System.Windows.Forms.PictureBox pbError;
+        private System.Windows.Forms.TextBox tbLetter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lUsed;
+    }
+}
+
---
+++ Hangman/Hangman/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hangman
+{
+    public partial class Form1 : Form
+    {
+        string originalWord = "программирование";
+        int errorCount = 0;
+        string usedLetters = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Restart()
+        {
+            string tmp = "";
+            for (int i = 0; i < originalWord.Length; i++) tmp += '*';
+            lMasked.Text = tmp;
+            errorCount = 0;
+            usedLetters = "";
+            lUsed.Text = "";
+            pbError.Image = Image.FromFile("images/0.png");
+            tbLetter.Text = "";
+            tbLetter.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Restart();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string letter = tbLetter.Text.Trim().ToLower();
+            if (letter.Length == 1 && letter[0] >= 'а' && letter[0] <= 'я' && usedLetters.IndexOf(letter[0])<0) {
+                usedLetters += letter;
+                lUsed.Text = usedLetters;
+                if (originalWord.IndexOf(letter[0]) < 0)
+                {
+                    errorCount++;
+                    pbError.Image = Image.FromFile("images/" + errorCount + ".png");
+                    if (errorCount == 6) {
+                        MessageBox.Show("К сожалению, вы проиграли(", "Результат");
+                        Restart();
+                    }
+                }
+                else {
+                    for (int i = 0; i < originalWord.Length; i++)
+                    {
+                        if (originalWord[i] == letter[0])
+                        {
+                            lMasked.Text=lMasked.Text.Insert(i, letter);
+                            lMasked.Text=lMasked.Text.Remove(i + 1, 1);
+                            //lMasked.Text = lMasked.Text.Substring(0, i) + letter[0] + lMasked.Text.Substring(i + 1, lMasked.Text.Length - i - 1);
+                        }
+                    }
+                    if (lMasked.Text.IndexOf('*') < 0) {
+                        MessageBox.Show("Ура!!! Вы выиграли)", "Результат");
+                        Restart();
+                    }
+                }
+            }
+            tbLetter.Focus();
+            tbLetter.Text = "";
+        }
+
+        private void tbLetter_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == 13) button1_Click(sender, e);
+        }
+    }
+}
